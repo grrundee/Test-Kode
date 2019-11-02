77e6950d3a69c1ad6e9063e950afba003e11526c
Grunde <grunde27@gmail.com>
2019-11-02 20:46:29 +0100

    Lagt til bortkommentert kode for å utføre stored procedure

--- DatoTimeTest/DatoTimeTest/UserControl1.Designer.cs
+++ DatoTimeTest/DatoTimeTest/UserControl1.Designer.cs
@@ -31,13 +31,14 @@
             this.button1 = new System.Windows.Forms.Button();
             this.txtInput = new System.Windows.Forms.TextBox();
             this.txtShow = new System.Windows.Forms.TextBox();
+            this.btnSendDate = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(283, 120);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(242, 27);
+            this.button1.Size = new System.Drawing.Size(242, 36);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,23 +46,35 @@
             // 
             // txtInput
             // 
-            this.txtInput.Location = new System.Drawing.Point(283, 175);
+            this.txtInput.Location = new System.Drawing.Point(283, 179);
             this.txtInput.Name = "txtInput";
             this.txtInput.Size = new System.Drawing.Size(242, 26);
             this.txtInput.TabIndex = 1;
             // 
             // txtShow
             // 
-            this.txtShow.Location = new System.Drawing.Point(283, 229);
+            this.txtShow.Location = new System.Drawing.Point(283, 228);
             this.txtShow.Name = "txtShow";
             this.txtShow.ReadOnly = true;
             this.txtShow.Size = new System.Drawing.Size(242, 26);
             this.txtShow.TabIndex = 1;
             // 
+            // btnSendDate
+            // 
+            this.btnSendDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSendDate.Location = new System.Drawing.Point(283, 61);
+            this.btnSendDate.Name = "btnSendDate";
+            this.btnSendDate.Size = new System.Drawing.Size(242, 36);
+            this.btnSendDate.TabIndex = 2;
+            this.btnSendDate.Text = "Send dato til database";
+            this.btnSendDate.UseVisualStyleBackColor = true;
+            this.btnSendDate.Click += new System.EventHandler(this.btnSendDate_Click);
+            // 
             // UserControl1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnSendDate);
             this.Controls.Add(this.txtShow);
             this.Controls.Add(this.txtInput);
             this.Controls.Add(this.button1);
@@ -77,5 +90,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox txtInput;
         private System.Windows.Forms.TextBox txtShow;
+        private System.Windows.Forms.Button btnSendDate;
     }
 }
--- DatoTimeTest/DatoTimeTest/UserControl1.cs
+++ DatoTimeTest/DatoTimeTest/UserControl1.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace DatoTimeTest
 {
     public partial class UserControl1: UserControl
     {
+        // string connectionString = ConfigurationManager.ConnectionStrings["conRailroad"].ConnectionString;//Lagrer connection string til en variabel
+        // SqlConnection con = new SqlConnection("connectionString");//Oppretter et objekt av klassen sqlConnection
+
         public UserControl1()
         {
             InitializeComponent();
@@ -22,6 +27,19 @@ namespace DatoTimeTest
             DateTime dato = new DateTime(); // Oppretter et objekt av DateTime klassen
             dato = Convert.ToDateTime(txtInput.Text); // konverterer innholdet i textboxen til DateTime
             txtShow.Text = dato.ToShortDateString(); // konverter til string og dropper tids delen
+        }
+
+        private void btnSendDate_Click(object sender, EventArgs e)
+        {
+            /*DataTable dt = new DataTable();
+            SqlCommand procedure = new SqlCommand("navnet på proceduren", con); 
+            procedure.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter sda;
+            con.Open();
+            sda = new SqlDataAdapter(procedure);
+            sda.Fill(dt); 
+            procedure.ExecuteNonQuery();
+            con.Close();*/
         }
     }
 }
